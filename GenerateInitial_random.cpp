aedcafc665f6afc3a567f5971132a01962568d23
dedede_dede <dedede_dede@outlook.jp>
2020-01-17 00:59:57 +0900

    ちょい修正2

--- GenerateInitial_random.cpp
+++ GenerateInitial_random.cpp
@@ -4,7 +4,6 @@
 
 const long        MAX_POINT    = 10000;
 const long        MAX_VELOCITY = 100;
-//const long        MAX_MASS     = 10000;
 const long        N            = 10000;
 const std::string FILE_NAME    = "inputfile.dat";
 
@@ -16,7 +15,6 @@ int main() {
     std::random_device seed;
     std::mt19937 rnd_p(seed());
     std::mt19937 rnd_v(seed());
-    //std::mt19937 rnd_m(seed());
     std::ofstream ofs(FILE_NAME);
     ofs << 0 << " ";
     for (int i = 0; i < N; i++) {
@@ -24,7 +22,6 @@ int main() {
         ofs << getSuitableRnd(rnd_p, rnd_v, MAX_POINT) << " ";
         ofs << getSuitableRnd(rnd_v, rnd_p, MAX_VELOCITY) << " ";
         ofs << getSuitableRnd(rnd_v, rnd_p, MAX_VELOCITY) << " ";
-        //ofs << (rnd_m()%MAX_MASS)+1 << " ";
     }
     return 0;
 }
