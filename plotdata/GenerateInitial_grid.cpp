57969e77c27306dc4e472081ab603dd1c5c45c8f
dedede_dede <dedede_dede@outlook.jp>
2020-01-27 17:23:21 +0900

    generateInitial_grid.cppの値域修正

--- plotdata/GenerateInitial_grid.cpp
+++ plotdata/GenerateInitial_grid.cpp
@@ -1,9 +1,10 @@
-//速度と質量はとりあえず固定
-//仕様にそって一直線出力に
+//速度はとりあえず固定
+//-MAX_POINT<=x,y<=MAX_POINT
 #include <iostream>
 #include <random>
 #include <fstream>
 #include <cmath>
+#include <string>
 
 const long        MAX_POINT          = 100;
 const long        INITIAL_VELOCITY_X = 0;
@@ -12,10 +13,11 @@ const long        N                  = 100;
 const std::string FILE_NAME          = "Initialplot_grid.dat";
 
 int main() {
-    long lines, x, y, dx, dy, count;
+    long lines, count;
+    double x, y, dx, dy;
     lines = std::ceil(std::sqrt((double)N));
-    dx = dy = MAX_POINT/lines;
-    x = y = 0;
+    dx = dy = 2*MAX_POINT/lines;
+    x = y = -MAX_POINT;
     count = 0;
     std::ofstream output(FILE_NAME);
     for (int i = 0; i < lines; i++) {
@@ -26,7 +28,7 @@ int main() {
             if (count == N) break;
             x += dx;
         }
-        x = 0;
+        x = -MAX_POINT;
         y += dy;
     }
     output.close();
