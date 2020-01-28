a38835ba16ab632a242d9b9dd87530676b7f22bc
dedede_dede <dedede_dede@outlook.jp>
2020-01-28 16:40:28 +0900

    generateInitial_circle.cppを(Θ,r)に作り直し

--- plotdata/generateInitial_circle.cpp
+++ plotdata/generateInitial_circle.cpp
@@ -1,23 +1,26 @@
 //座標半径MAX_POINTの円の中
 //速度x,yそれぞれ[-MAX_VEROCITY,MAX_VEROCITY]
-//x,y独立じゃない乱数になっちゃったのでΘ,rの乱数を座標変換するかんじで作り直したほうがいいかも
 //一応出力の形式をゆーたんのgridにあわせました
 #include <iostream>
 #include <random>
 #include <fstream>
 #include <cmath>
+#include <math.h>
 
-const long        MAX_POINT     = 1;
+#define PI 3.14159265359
+
+const long        MAX_RADIUS    = 1;
+const double      MAX_THETA     = 2*PI;
 const long        MAX_VELOCITY  = 1;
 const long        N             = 100;
-const long        DECIMAL_DEGIT = 8;
+const long        DECIMAL_DEGIT = 6;
 const std::string FILE_NAME     = "Initialplot_circle.dat";
 
 int sign(int input); //乱数の入力を1or-1で返すだけ
 
 double get0toMaxRnd(std::mt19937 &rnd, double max);
 
-void outputCircleRnd(std::ofstream &ofs, std::mt19937 &output, std::mt19937 &rnd, long max);
+void outputCircleRnd(std::ofstream &ofs, std::mt19937 &output);
 
 int main() {
     std::random_device seed;
@@ -25,7 +28,7 @@ int main() {
     std::mt19937 rnd_v(seed());
     std::ofstream ofs(FILE_NAME);
     for (int i = 0; i < N; i++) {
-        outputCircleRnd(ofs, rnd_p, rnd_v, MAX_POINT);
+        outputCircleRnd(ofs, rnd_p);
         ofs << sign(rnd_p())*get0toMaxRnd(rnd_v, MAX_VELOCITY) << " ";
         ofs << sign(rnd_p())*get0toMaxRnd(rnd_v, MAX_VELOCITY) << " ";
         ofs << std::endl;
@@ -37,7 +40,6 @@ int sign(int input) {
     return (input%2) ? 1 : -1;
 }
 
-
 //0からmaxまで,小数第DECIMAL_DEGIT位までの乱数
 double get0toMaxRnd(std::mt19937 &rnd, double max) {
     long pow10digit  = std::pow(10, DECIMAL_DEGIT);
@@ -45,9 +47,12 @@ double get0toMaxRnd(std::mt19937 &rnd, double max) {
     return (double)(rnd()%maxInteger)/pow10digit;
 }
 
-void outputCircleRnd(std::ofstream &ofs, std::mt19937 &output, std::mt19937 &rnd, long radius) {
-    double x    = sign(rnd())*get0toMaxRnd(output, 1);
-    double ymax = std::sqrt(1 - x*x);
-    double y    = sign(rnd())*get0toMaxRnd(output, ymax);
+void outputCircleRnd(std::ofstream &ofs, std::mt19937 &output) {
+    double r     = get0toMaxRnd(output, MAX_RADIUS);
+    double theta = get0toMaxRnd(output, MAX_THETA);
+    double x = r*std::cos(theta);
+    double y = r*std::sin(theta);
     ofs << x << " " << y << " ";
+
+    std::cout << "test" << x*x+y*y << std::endl;
 }
