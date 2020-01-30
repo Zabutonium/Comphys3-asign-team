f537be08db1ecca768acb72c9b14348f27199e72
Uta Hiemori <uhie@Zabutonium.local>
2020-01-30 13:59:44 +0900

    初期条件を平行移動させる関数を導入。

--- plotdata/mainplot.cpp
+++ plotdata/mainplot.cpp
@@ -10,10 +10,14 @@
 #define string std::string
 
 const long N           = 5000;  //particleの数
-const double MAX_TIME    = 0.5;   //tの最大値
+const double MAX_TIME    = 5;   //tの最大値
 const long DEVIDE_TIME = 100;  //tを何分割するか =n
 const double SOFTING_FORCE    = 0.01;  //力のsofting parameter
-const double CORRECT_VELOCITY = 1;     //速度の補正値
+const double CORRECT_VELOCITY = 0.6;     //速度の補正値
+const double MOVE_x             = 0.0;    //初期条件の座標をずらす
+const double MOVE_y             = 0.0;
+const double MOVE_Vx             = 0.0;    //初期条件の速度をずらす
+const double MOVE_Vy             = 0.0;
 const string FILE_NAME = "Initialplot_circle.dat";
 
 const double dt = (double)MAX_TIME / DEVIDE_TIME;
@@ -30,6 +34,8 @@ struct object {
 
     object(void) {}
 };
+
+void MoveCollision(vector<object> &per);
 
 void correctVelocity(vector<object> &per);
 
@@ -56,6 +62,7 @@ int main() {
     vector<object> particle(N);
     inputFromFile(particle);
     correctVelocity(particle);
+    MoveCollision(particle);
     for (int i = 0; i <= DEVIDE_TIME; i++) {
         double t = i*dt;
         outputToFile(particle, t,i);
@@ -63,6 +70,7 @@ int main() {
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////
+
 
 void inputFromFile(vector<object> &per) {
     std::cout << "start reading ..." << std::endl;
@@ -96,6 +104,23 @@ void correctVelocity(vector<object> &per){
         per[i].v[1] *= CORRECT_VELOCITY;
     }
 }
+
+void MoveCollision(vector<object> &per){
+    for(int i = 0; i < N; i++){
+        if(i % 2 == 0){
+            per[i].x[0] += MOVE_x;
+            per[i].x[1] += MOVE_y;
+            per[i].v[0] -= MOVE_Vx;
+            per[i].v[1] -= MOVE_Vy;
+        }else if(i % 2 == 1){
+            per[i].x[0] -= MOVE_x;
+            per[i].x[1] -= MOVE_y;
+            per[i].v[0] += MOVE_Vx;
+            per[i].v[1] += MOVE_Vy;
+        }
+    }
+}
+
 
 double distance(object obj1, object obj2) {
     return std::sqrt(pow(obj1.x[0] - obj2.x[0] , 2) + pow(obj1.x[1] - obj2.x[1] , 2));
